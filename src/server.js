5c802a418400eaf784ae35ff00605b633b192b50
AntoineThomasCode <antoinethomas.devweb@gmail.com>
2026-02-05 10:54:44 +0100

    feat: add Markdown rendering for chatbot responses

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
       scriptSrcAttr: ["'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'"],
